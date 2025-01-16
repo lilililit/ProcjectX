16a0ff25d267e0d80d8b58db1c8e196ac9492052
lilililit <rokogamesjp@gmail.com>
2025-01-16 02:59:17 +0100

    fix

---
+++ ProjectXxX/Assets/HealthBarclip.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class HealthBarBillboard : MonoBehaviour
+{
+    public Camera playerCamera;  // Kamera gracza
+
+    void Update()
+    {
+        if (playerCamera != null)
+        {
+            // Obróæ Canvas, aby by³ zawsze zwrócony w stronê kamery
+            transform.LookAt(transform.position + playerCamera.transform.rotation * Vector3.forward,
+                             playerCamera.transform.rotation * Vector3.up);
+        }
+    }
+}
